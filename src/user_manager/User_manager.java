6e3ff1b5cceff58ddb976f1a78b3deb16c344528
Gerges hanna FCI-H <gogohanna50@gmail.com>
2020-03-20 11:54:01 +0200

    user

---
+++ src/user_manager/User_manager.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package user_manager;
+
+/**
+ *
+ * @author Gerges hanna FCI-H
+ */
+public class User_manager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("test");
+    }
+    
+}
